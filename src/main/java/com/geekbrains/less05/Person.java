1b8625d1bf51bb1f8b8f58c286b39f74961a4cd7
msvdev-k <msvdev.k@gmail.com>
2021-11-18 17:38:11 +0300

    add class fields

--- src/main/java/com/geekbrains/less05/Person.java
+++ src/main/java/com/geekbrains/less05/Person.java
@@ -2,5 +2,17 @@ package com.geekbrains.less05;
 
 public class Person {
 
+    /* Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.*/
+
+    private String name;
+    private String function;
+    private String email;
+    private String phone;
+    private double salary;
+    private int age;
+
+    
+
+
 
 }
