8aa0300025fd5b0b82997e494acf8d4968b75d0c
msvdev-k <msvdev.k@gmail.com>
2021-11-21 19:58:31 +0300

    add class Dog

---
+++ src/main/java/com/geekbrains/less06/Dog.java
@@ -0,0 +1,21 @@
+package com.geekbrains.less06;
+
+
+public class Dog extends Animal {
+
+    // Количество собак
+    private static int dogCount = 0;
+
+
+    public Dog(String name) {
+        super(name, 500, 10);
+
+        dogCount++;
+    }
+
+
+    public static int getCount() {
+        return dogCount;
+    }
+
+}
