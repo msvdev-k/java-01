7558c626c66dd1f6a46d1ce903ad5dac38dc6935
msvdev-k <msvdev.k@gmail.com>
2021-11-26 21:55:46 +0300

    fix class Cat

--- src/main/java/com/geekbrains/less07/Cat.java
+++ src/main/java/com/geekbrains/less07/Cat.java
@@ -6,16 +6,35 @@ public class Cat {
 
     private final int appetite;
 
+    private boolean satiety;
+
+
 
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
+        this.satiety = false;
     }
 
 
 
     public void eat(Plate p) {
-        p.decreaseFood(appetite);
+
+        if (satiety) {
+            System.out.printf("%s не хочет есть.", name);
+
+        } else {
+
+            satiety = p.decreaseFood(appetite);
+
+            if (satiety) {
+                System.out.printf("%s наелся(ась).", name);
+            } else {
+                System.out.printf("%s не смог(ла) поесть. В тарелке мало еды.", name);
+            }
+        }
+        System.out.println();
+
     }
 
 
