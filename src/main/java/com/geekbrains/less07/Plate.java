b1f2bd9dcc8b88cd67cd30053600ddc351096395
msvdev-k <msvdev.k@gmail.com>
2021-11-26 21:13:11 +0300

    add method decreaseFood to class Plate

--- src/main/java/com/geekbrains/less07/Plate.java
+++ src/main/java/com/geekbrains/less07/Plate.java
@@ -5,11 +5,14 @@ public class Plate {
     private int food;
 
 
-
     public Plate(int food) {
         this.food = food;
     }
-    
+
+
+    public void decreaseFood(int n) {
+        food -= n;
+    }
 
 
     public void info() {
