0d1e2ec257fa1448451478a9db06474e136c8436
msvdev-k <msvdev.k@gmail.com>
2021-11-26 21:20:22 +0300

    fix main method

--- src/main/java/com/geekbrains/less07/Main.java
+++ src/main/java/com/geekbrains/less07/Main.java
@@ -3,5 +3,14 @@ package com.geekbrains.less07;
 public class Main {
     public static void main(String[] args) {
 
+        Cat cat = new Cat("Барсик", 5);
+        Plate plate = new Plate(100);
+
+        plate.info();
+        
+        cat.eat(plate);
+
+        plate.info();
+
     }
 }
