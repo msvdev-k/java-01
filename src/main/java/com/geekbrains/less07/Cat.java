39a575a7cb37c1d619399ed0e765eaa0d28d563f
msvdev-k <msvdev.k@gmail.com>
2021-11-26 21:01:05 +0300

    add class Cat

---
+++ src/main/java/com/geekbrains/less07/Cat.java
@@ -0,0 +1,27 @@
+package com.geekbrains.less07;
+
+public class Cat {
+
+    private final String name;
+
+    private final int appetite;
+
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+
+
+    public void eat() {
+
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/main/java/com/geekbrains/less07/Main.java
+++ src/main/java/com/geekbrains/less07/Main.java
@@ -2,6 +2,6 @@ package com.geekbrains.less07;
 
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
